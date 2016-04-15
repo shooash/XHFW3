bdfda20157e5d1a0287a6bb176cab95aacb829da
shoo_ash <ash.mail@mail.ru>
2016-04-15 21:59:24 +0300

    Bigger floating dot size

--- src/com/zst/xposed/halo/floatingwindow3/floatdot/FloatingDot.java
+++ src/com/zst/xposed/halo/floatingwindow3/floatdot/FloatingDot.java
@@ -49,7 +49,7 @@ public class FloatingDot implements Runnable
 
     public FloatingDot(Context sContext) {
 		mContext = sContext;
-		mCircleDiameter = Util.realDp(24, mContext);
+		mCircleDiameter = Util.realDp(32, mContext);
 	}
 
 	
