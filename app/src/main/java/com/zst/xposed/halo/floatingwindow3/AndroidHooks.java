9e515d19a97b5ba9a6c04849ae49ef7c9bb28a19
shooash <ash.inbox@gmail.com>
2016-06-04 17:38:35 +0300

    [fix] whatsapp bug

--- app/src/main/java/com/zst/xposed/halo/floatingwindow3/AndroidHooks.java
+++ app/src/main/java/com/zst/xposed/halo/floatingwindow3/AndroidHooks.java
@@ -375,7 +375,11 @@ public class AndroidHooks
 //					isMovable = isMovable || (mIntent!=null&&Util.isFlag(mIntent.getFlags(), MainXposed.mPref.getInt(Common.KEY_FLOATING_FLAG, Common.FLAG_FLOATING_WINDOW)));
 					String packageName = Util.getFailsafeStringFromObject(null, previous, "packageName");
 					isMovable = MWRegister.isMovable(packageName);
+					if(packageName.equals("com.whatsapp"))
+						isMovable = false;
 					if (!isMovable) return;
+					
+					
 					//if (mIsPreviousActivityHome) return;
 
 //					Object nextAR = XposedHelpers.callMethod(param.thisObject, "topRunningActivityLocked",
